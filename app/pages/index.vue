72913544f9f317ac08b117184bc6bf5de8fb213f
rararat <118641555+ppReyy@users.noreply.github.com>
2023-01-03 11:52:19 +0700

    Add files via upload

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,8 +3,8 @@
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
+
         <div class="mb-12 xl:mb-0">
-        
           <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
 
           <form
@@ -32,8 +32,6 @@
             </button>
           </form>
         </div>
-
-        
       </div>
       <div class="flex flex-col w-full xl:w-2/5">
         <img
